714032009fef8f02bf8d40dc206049c3736ea77c
Kevin Sutton <kevmsutton@gmail.com>
2018-11-06 11:05:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(fizzNum)
   if fizzNum % 3 && fizzNum % 5 == 0
     return "FizzBuzz"
   elsif fizzNum % 3 == 0
-    return "Buzz"
-  elsif fizzNum % 5 == 0
     return "Fizz"
+  elsif fizzNum % 5 == 0
+    return "Buzz"
   else
     return nil
 end
